0a5e7230a632983735dea253e292060ee66d95e6
kik3078 <kik3078@naver.com>
2019-07-31 22:30:42 +0900

    #3.3 completed

--- README.md
+++ README.md
@@ -3,7 +3,10 @@ insta-clone
 
 
 ## User Stories
-- [ ] See the feed
+
+- [x] Create account
+- [ ] Request Secret
+- [ ] Confirm Secret (Login)
 - [ ] Like / Unlike a photo
 - [ ] Comment on a photo
 - [ ] Search by user
@@ -12,5 +15,7 @@ insta-clone
 - [ ] Follow / Unfollow User
 - [ ] See the full photo
 - [ ] Edit my profile
-- [ ] Create account
 - [ ] Log in
+- [ ] Upload a photo
+- [ ] Edit the photo (Delete)
+- [ ] See the feed
--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
-require("dotenv").config()
-import { GraphQLServer } from 'graphql-yoga';
+import dotenv from "dotenv"
+import path from "path";
+dotenv.config({path: path.resolve(__dirname, ".env")})
+import { GraphQLServer } from "graphql-yoga";
 import loggar from "morgan";
 import schema from "./schema"
+import { sendSecretMail } from "./utils"
+
+sendSecretMail("kik3078@naver.com", "123")
 
 const PORT = process.env.PORT || 4000;
 
